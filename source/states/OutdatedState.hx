fb9443fb5c6218ee31479e51b2ec26c637a81501
Daves The Bone <davesthebone@gmail.com>
2024-11-07 20:16:59 +0900

    Update OutdatedState.hx

--- source/states/OutdatedState.hx
+++ source/states/OutdatedState.hx
@@ -14,7 +14,7 @@ class OutdatedState extends MusicBeatState
 
 		warnText = new FlxText(0, 0, FlxG.width,
 			"Sup bro, looks like you're running an   \n
-			outdated version of Symphony Engine (" + MainMenuState.symphonyEngineVersion + "),\n
+			outdated version of Psych Engine (" + MainMenuState.psychEngineVersion + "),\n
 			please update to " + TitleState.updateVersion + "!\n
 			Press ESCAPE to proceed anyway.\n
 			\n
@@ -30,7 +30,7 @@ class OutdatedState extends MusicBeatState
 		if(!leftState) {
 			if (controls.ACCEPT) {
 				leftState = true;
-				CoolUtil.browserLoad("https://github.com/Ruthenium-gangs/FNF-SymphonyEngne/releases");
+				CoolUtil.browserLoad("https://github.com/ShadowMario/FNF-PsychEngine/releases");
 			}
 			else if(controls.BACK) {
 				leftState = true;
